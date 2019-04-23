a520ff34ef72d67e7c9825d930dd7166533450a5
Alexey Sednev <asednev@metromile.com>
2019-04-23 09:51:02 -0700

    aws profiles

--- src/common/database/datamapper/datamapper.service.ts
+++ src/common/database/datamapper/datamapper.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { DataMapper } from '@aws/dynamodb-data-mapper';
+import * as AWS from 'aws-sdk';
 import DynamoDB = require('aws-sdk/clients/dynamodb');
 import { ConfigService } from 'src/common/config/config.service';
 
 enum AWSConfigOptions {
   Region = 'AWS.Region',
+  Profile = 'AWS.Profile',
 }
 
 @Injectable()
@@ -13,6 +15,14 @@ export class DataMapperService {
   private _mapper: DataMapper;
 
   constructor(private config: ConfigService) {
+
+    const awsProfile = config.get(AWSConfigOptions.Profile);
+    if (awsProfile) {
+      // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html
+      const credentials = new AWS.SharedIniFileCredentials({profile: awsProfile});
+      AWS.config.credentials = credentials;
+    }
+
     this._mapper = new DataMapper({
       client: new DynamoDB({ region: config.get(AWSConfigOptions.Region) }), // the SDK client used to execute operations
       tableNamePrefix: '', // optionally, you can provide a table prefix to keep your dev and prod tables separate
